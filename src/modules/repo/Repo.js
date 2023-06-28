a3a96afe819a95c4ecc7340951ad625d767b3522
Benaya Trabelsi <benaya7@gmail.com>
2023-06-28 14:43:15 +0300

    fix link

--- src/modules/repo/Repo.js
+++ src/modules/repo/Repo.js
@@ -7,7 +7,7 @@ const Repo = ({ repo }) => (
     <div className={styles.repo}>
       <h2><a href={repo.html_url} target="_blank" rel="noopener noreferrer">{repo.name}</a></h2>
       <p>{repo.description}</p>
-      {repo.homepage && <a href='link to medium article}'>{repo.homepage}</a>}
+      {repo.homepage && <a href={repo.homepage}>link to medium article</a>}
       <p>Language: {repo.language}</p>
       {repo.stargazers_count>0 && <p>Stars: {repo.stargazers_count}</p>}
       {repo.watchers_count>0 && <p>Watchers: {repo.watchers_count}</p>}
