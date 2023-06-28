9d77839ced129fab35be36c500905ccc2032e0c0
Benaya Trabelsi <benaya7@gmail.com>
2023-06-28 14:45:00 +0300

    added topics if exist

--- src/modules/repo/Repo.js
+++ src/modules/repo/Repo.js
@@ -9,6 +9,7 @@ const Repo = ({ repo }) => (
       <p>{repo.description}</p>
       {repo.homepage && <a href={repo.homepage}>link to medium article</a>}
       <p>Language: {repo.language}</p>
+      {repo.topics.length>0 && <p>Topics: {repo.topics.join(', ')}</p>}
       {repo.stargazers_count>0 && <p>Stars: {repo.stargazers_count}</p>}
       {repo.watchers_count>0 && <p>Watchers: {repo.watchers_count}</p>}
     </div>
